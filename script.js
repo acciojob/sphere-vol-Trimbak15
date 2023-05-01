94ba7123096ebaacd3a9fb6e2d4e57cfaecbf876
Trimbak Pakhale <118504736+Trimbak15@users.noreply.github.com>
2023-05-01 18:11:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,18 +1,12 @@
-function calculateVolume() {
-      // Get the value of the radius input field
-      let radius = document.getElementById("radius").value;
+function volume_sphere() {
+            // get the radius input value
+            const radiusInput = document.getElementById("radius");
+            const radius = parseFloat(radiusInput.value);
 
-      // Calculate the volume of the sphere
-      let volume = volume_sphere(radius);
+            // calculate the volume of the sphere
+            const volume = (4 / 3) * Math.PI * Math.pow(radius, 3);
 
-      // Display the volume in the output field
-      document.getElementById("volume").value = volume;
-    }
-
-    function volume_sphere(radius) {
-      // Calculate the volume of a sphere given its radius
-      let volume = (4/3) * Math.PI * Math.pow(radius, 3);
-      return volume;
-    }
-
-window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
+            // display the volume in the volume field
+            const volumeField = document.getElementById("volume");
+            volumeField.value = volume.toFixed(2); // round to 2 decimal places
+        }
